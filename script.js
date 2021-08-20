55be0bab06ab7819ff04abf00ce5b1e744304cb9
ddMuyi <daodu.davidm@gmail.com>
2021-08-20 08:14:17 +0100

    animation

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
+(function() {
+  var elements;
+  var windowHeight;
+
+  function init() {
+    elements = document.querySelectorAll('.hidden');
+    windowHeight = window.innerHeight;
+  }
+
+  function checkPosition() {
+    for (var i = 0; i < elements.length; i++) {
+      var element = elements[i];
+      var positionFromTop = elements[i].getBoundingClientRect().top;
+
+      if (positionFromTop - windowHeight <= 0) {
+        element.classList.add('fade-in-element');
+        element.classList.remove('hidden');
+      }
+    }
+  }
+
+  window.addEventListener('scroll', checkPosition);
+  window.addEventListener('resize', init);
+
+  init();
+  checkPosition();
+})();
+
+
 $(function(){
   $(".burger").click(()=>{
     $(".nav-bar").fadeToggle();
   })
 })
+
+//animation
